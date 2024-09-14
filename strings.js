d59ae79af1ef6147389a3ab7cc32e3cda9e4505b
unknown <ceedriclazaro010205@gmail.com>
2024-09-14 15:41:32 -0800

    Initial commit with strings.js

---
+++ strings.js
@@ -0,0 +1,14 @@
+let favActorsFirstName = "Anne";
+let favActorsLastName = "Curtis";
+let fullName = favActorsFirstName + " " + favActorsLastName;
+console.log(favActorsFirstName);
+console.log(favActorsLastName);
+console.log(fullName);
+
+let uppercase = "anne curtis";
+console.log(uppercase.toUpperCase());
+let lowercase = "PANDA";
+console.log(lowercase.toLowerCase());
+
+let message = "My favorite actor is " + (fullName.toUpperCase()) + " her best show is Dyesebel";
+console.log(message)
